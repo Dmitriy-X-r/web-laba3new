174200f020c784dd06ceed6aecfba74a4692bd7c
Dima <batunin.diman2003@gmail.com>
2024-05-11 03:08:36 +0700

    laba3 is ready

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
-import { calc } from "./test";
+import "./components/App.css";
 
 console.log("Hello World!");
-console.log(calc(5, 6));
+
+function chSelected() {
+    const selectTypeValue = document.querySelector('#type').value;
+    if(selectTypeValue == "Пополняемый")
+    {
+        document.querySelector('#percent-two').style.display = 'none';
+        document.querySelector('#percent-one').style.display = 'block';
+    }
+    else
+    {
+        document.querySelector('#percent-one').style.display = 'none';
+        document.querySelector('#percent-two').style.display = 'block';
+    }
+  }
+
+function Calculate() {
+    const selectTypeValue =document.querySelector('#type').value;
+    var period;
+    if(selectTypeValue != ''){
+        if(selectTypeValue == "Пополняемый")
+            period = document.querySelector('#percent-one').value;
+        else if (selectTypeValue == "Срочный")
+            period = document.querySelector('#percent-two').value;
+        if(period != ""){
+            var valueArr = period.split(',').map(item => isNaN(item) ? item : parseFloat(item));
+            var moneySum = parseFloat(document.querySelector('#moneySum').value);
+            document.getElementById('id_out__text-info').innerHTML = "Вклад "+selectTypeValue+" на срок " + valueArr[0] + " на сумму "+ moneySum +" руб.";
+            document.getElementById('id_out__text-sum').innerHTML = ""+(moneySum+moneySum*valueArr[2]*valueArr[1]).toFixed(2)+" руб.";
+        }
+    }
+    else
+    {
+        document.getElementById('id_out__text-info').innerHTML = "";
+        document.getElementById('id_out__text-sum').innerHTML = "";
+    }
+  }
+
+document.getElementById('type').addEventListener('change', chSelected);
+document.querySelector('.form__calculator-button').addEventListener('click', Calculate);
--- src/test.js
+++ src/test.js
@@ -1,3 +1,17 @@
-export function calc(a, b) {
-    return a + b;
-}
+import "./components/second.css";
+
+const axios = require('axios').default;
+
+axios
+    .get('https://api.hh.ru/vacancies/98539502')
+    .then((response) => {
+        console.log(response.data);
+        let worker = document.querySelector('.worker').textContent = response.data.name;
+        // let salary = document.querySelector('.salary').textContent = "Зарплата: от " + response.data.salary.from + " до " + response.data.salary.to;
+        let salary = document.querySelector('.salary').textContent = "Зарплата: от " + response.data.salary.from;
+        let company = document.querySelector('.company').textContent = "Компани: " + response.data.employer.name;
+        let address = document.querySelector('.address').textContent = "Адресс: " + response.data.address.city + ", " + response.data.address.street;
+        let experience = document.querySelector('.experience').textContent = "Опыт работы: " + response.data.experience.name;
+    })
+    .catch((error) => { console.error(error); });
+console.log(response.data);
